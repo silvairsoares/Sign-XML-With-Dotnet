e548074b6c46e5e31300022513a19561e5274ffa
Silvair L. Soares <silvairsoares@outlook.com>
2022-02-04 11:07:28 -0300

    Update Program.cs

--- Sign_XML_With_Dotnet/Program.cs
+++ Sign_XML_With_Dotnet/Program.cs
@@ -14,7 +14,7 @@ namespace Sign_XML_With_Dotnet
             xmlNaoAssinado.PreserveWhitespace = false;
 
             // Load the passed XML file using it’s name.
-            ///xmlNaoAssinado.LoadXml("String completa do XML, ainda sem assinatura");
+            ///xmlNaoAssinado.LoadXml("Full XML string, still unsigned");
             xmlNaoAssinado.LoadXml("<?xml version=\"1.0\" encoding=\"UTF - 8\" ?> \n" +
                 "<NFe xmlns=\"http://www.contoso.com/books\"> \n" +
                 "  <infNFe versao=\"4.00\" Id=\"NFe52211005462662000105550010000041721855216620\" >\n" +
